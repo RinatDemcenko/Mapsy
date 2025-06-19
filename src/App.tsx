a43b51f0924e41ce0d68738e5863460f870729d5
RinatDemcenko <rinatdemcenko@gmail.com>
2025-06-19 15:07:05 +0200

    pushing boilerplate version

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-6xl font-bold mb-8 text-amber-200">
+        <h1>Mapsy</h1>
+      </div>
+      <div className="text-center text-lg">
+        <p>
+          This is a page where Mapsy, a non-commercial project for searching
+          points of interest near you will be available in the future.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
